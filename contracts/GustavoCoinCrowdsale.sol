2292c6b46ef93ca3728ef40a1297b09a4279f39d
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-07-12 18:15:18 +0200

    Change GustavoCoinCrowdsale code so it is deployed.

--- contracts/GustavoCoinCrowdsale.sol
+++ contracts/GustavoCoinCrowdsale.sol
@@ -6,24 +6,12 @@ import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
 contract GustavoCoinCrowdsale is Crowdsale {
 
-  GustavoCoin public token;
-
-  function GustavoCoinCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) {
-    require(_startBlock >= block.number);
-    require(_endBlock >= _startBlock);
-    require(_rate > 0);
-    require(_wallet != 0x0);
-
-    token = createTokenContract();
-    startBlock = _startBlock;
-    endBlock = _endBlock;
-    rate = _rate;
-    wallet = _wallet;
+  function GustavoCoinCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet) Crowdsale(_startBlock, _endBlock, _rate, _wallet) {
   }
 
   // creates the token to be sold.
   // override this method to have crowdsale of a specific MintableToken token.
-  function createTokenContract() internal returns (GustavoCoin) {
+  function createTokenContract() internal returns (MintableToken) {
     return new GustavoCoin();
   }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,4 @@
-var GustavoCoin = artifacts.require("./GustavoCoin.sol")
-var GustavoCoinCrowdsale = artifacts.require("./GustavoCoinCrowdsale")
+var GustavoCoinCrowdsale = artifacts.require("./GustavoCoinCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
   const startBlock = web3.eth.blockNumber + 2
@@ -7,6 +6,5 @@ module.exports = function(deployer, network, accounts) {
   const rate = new web3.BigNumber(1000)
   const wallet = web3.eth.accounts[0]
 
-  deployer.deploy(GustavoCoin)
   deployer.deploy(GustavoCoinCrowdsale, startBlock, endBlock, rate, wallet)
 };
